89e0e2cb7b2ebec9e83985b62707ef02a9223cd5
Fatima Alubaidi <fatima@Fatimas-MacBook-Air.local>
2024-01-24 20:43:24 +0000

    killing the cat

---
+++ Cat.java
@@ -0,0 +1,31 @@
+public class Cat{
+    private int numLives;
+
+    public Cat() {
+        this.numLives = 9;
+    }
+
+    public int getNumLives() {
+        return numLives;
+    }
+
+    public void kill() {
+        if (numLives > 0) {
+            numLives--;
+            System.out.println("Ouch!"); 
+            
+        } 
+        if (numLives == 0){
+            System.out.println("Dead");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cat ginger = new Cat();
+
+        while (ginger.getNumLives() > 0) {
+            ginger.kill();
+        }
+    }
+}
+
